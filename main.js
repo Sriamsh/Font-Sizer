c910f1a988336096349ad5eb8553a67978df177c
Sriamsh <100685282+Sriamsh@users.noreply.github.com>
2023-01-17 19:53:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-function setup()
-{
-    video=createCapture(VIDEO);
-    video.size(550,550);
-    canvas=createCanvas(550,550);
-    canvas.position(560,150);
-    poseNet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
-}
 
+difference=0;
+leftWristX=0;
+rightWristX=0;
+function setup() 
+{
+   video = createCapture(VIDEO);
+   video.size(550,550);
+   canvas = createCanvas(550,550);
+   canvas.position(560,150);
+   poseNet=ml5.poseNet(video,modelLoaded);
+   poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
+}
 function draw()
 {
-}
-
-function modelLoaded()
-{
-    console.log("PoseNet Is Initialized");
+    background("red");
+    text("Sriamsh",200,200);
+    textSize(difference);
 }
 
 function gotPoses(results)
 {
-if(results.length>0)
-{
+  if(results.length>0)
+  {
     console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    difference=floor(leftWristX-rightWristX);
+    console.log("LeftWristX = "+leftWristX+"RightWristX = "+rightWristX+"difference = "+difference);
+  }
+
 }
-}
+
